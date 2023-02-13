7cdab1aacb2a278f6df9100cebcff3a8783169d5
ANANAzZzZz <vladscooltrol@gmail.com>
2023-02-13 11:48:41 +0300

    upload right version of lab_4 asm programming

--- course_2/Assembler_programming_basics/Lab_4_Embedded_program_development/main.cpp
+++ course_2/Assembler_programming_basics/Lab_4_Embedded_program_development/main.cpp
@@ -1,56 +1,49 @@
 #include<iostream>
 #include<locale>
+
 using namespace std;
-typedef signed char Array_type;
-const int mas_size = 20;
+typedef signed char arrayType;
+const int maxArraySize = 20;
+
 int main() {
-  int flag = 0;
   setlocale(LC_ALL, "ru");
-  int user_size = 0;
-  Array_type Arr[mas_size] = { 0 };
-  cout << "задание: Задан массив чисел со знаком размером в байт.\n" <<
-       " Написать приложение делающее сортировку по убыванию.\n" <<
-       "введите размер вводимого массива меньший " << mas_size << endl
+
+  int* maxEl = 0;
+  int inputSize = 0;
+
+  arrayType arr[maxArraySize] = {0};
+
+  cout << "задание: Задан массив чисел размером в байт.\n" <<
+       "Найти максимальный элемент массива.\n" <<
+       "введите размер вводимого массива меньший " << maxArraySize << endl
        << " -> ";
-  cin >> user_size;
+
+  cin >> inputSize;
+
   cout << "введите через пробел элементы массива\n" <<
        "в диапазоне от -128 до +127\n";
-  for (int i = 0; i < user_size; ++i) {
+
+  for (int i = 0; i < inputSize; ++i) {
     int tmp;
     std::cin >> tmp;
-    signed char Perem = tmp;
-    *(Arr + i) = Perem;
+    signed char a = tmp;
+    *(arr + i) = a;
   }
-
-  __asm__  (
-  "movl flag, $1h"
-  lea ebx, Arr
-  _while :
-  mov flag, 0
-  mov ecx, user_size
-  dec ecx
-  mov edi, 0
-  _L :
-  mov dl, [ebx + edi]
-  mov dh, [ebx + edi + 1]
-  cmp dl, dh
-  JA _no_change
-  xchg dl, dh
-  mov[ebx + edi], dl
-  mov[ebx + edi + 1], dh
-  mov flag, 1
-  _no_change:
-  inc di
-  Loop _L
-  cmp flag, 0
-  JNE _while
-  )
-
-  cout << "\nОтсортированный массив:\n";
-  for (int i = 0; i < user_size; ++i) {
-    int Perem = *(Arr + i);
-    cout << Perem << " ";
+  __asm {
+  mov ecx, inputSize
+  lea esi, arr
+  mov ebx, 0
+  _m1:
+    lodsb
+    cmp al, bh
+    jg _max
+    jmp _m2
+  _max:
+    mov bh, al
+  _m2:
+    loop _m1
+    mov maxEl, ebx
   }
-  cout << endl;
+  cout << "\n Максимальный элемент массива: \n" << maxEl;
   return 0;
 }
